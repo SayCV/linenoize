a56f36cd5e69df71f7bee804e032515aff361d53
SayCV <sssxyc@163.com>
2026-01-15 23:26:54 +0800

    minor changes

--- src/term.zig
+++ src/term.zig
@@ -8,10 +8,10 @@ const is_windows = builtin.os.tag == .windows;
 const termios = if (!is_windows) std.posix.termios else struct { inMode: w.DWORD, outMode: w.DWORD };
 
 pub fn isUnsupportedTerm(init: std.process.Init, allocator: std.mem.Allocator) bool {
+    _ = allocator;
     const env_map = init.environ_map;
-    defer env_map.deinit();
     const env_var = env_map.get("TERM") orelse return false;
-    defer allocator.free(env_var);
+
     return for (unsupported_term) |t| {
         if (std.ascii.eqlIgnoreCase(env_var, t))
             break true;
